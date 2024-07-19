f30a0767b976c10e9539ef7347a0f76ff3575286
nanakwametuffour <nanakwametuffour44@gmail.com>
2024-07-19 15:51:55 -0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
-
-
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+   dotenv.config()
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("Connect to db");
+  })
+  .catch(() => {
+    console.log("Not connect to db");
+  });
 
 
 
