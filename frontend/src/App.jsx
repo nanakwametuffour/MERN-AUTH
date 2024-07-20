4bd8e40710b4ff35607d48c939de17efb56b54c7
nanakwametuffour <nanakwametuffour44@gmail.com>
2024-07-20 02:08:45 -0700

    create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
     <>
-     <BrowserRouter>
-       <Header/>
-       <Routes>
-
-       </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/sign-in" element={<SignIn/>} />
+           <Route path='/profile' element={<Profile/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
